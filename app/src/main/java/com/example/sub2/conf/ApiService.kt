98321e82e6baa37577583384aac60459fa995b08
4nwar <sadad279@gmail.com>
2022-04-28 22:49:11 +0700

    token pole

--- app/src/main/java/com/example/sub2/conf/ApiService.kt
+++ app/src/main/java/com/example/sub2/conf/ApiService.kt
@@ -9,25 +9,25 @@ import retrofit2.http.*
 interface ApiService {
 
     @GET ("search/users")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token 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")
     fun getSearchUser(
         @Query ("q") query: String
     ):Call<UserResp>
 
     @GET ("users/{username}")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token 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")
     fun getUserdetail(
         @Path ("username")  username : String
     ):Call<UserDetailResp>
 
     @GET ("users/{username}/followers")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token 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")
     fun getFollowers(
         @Path ("username")  username : String
     ):Call<ArrayList<User>>
 
     @GET ("users/{username}/following")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token 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")
     fun getFollowing(
         @Path ("username")  username : String
     ):Call<ArrayList<User>>
